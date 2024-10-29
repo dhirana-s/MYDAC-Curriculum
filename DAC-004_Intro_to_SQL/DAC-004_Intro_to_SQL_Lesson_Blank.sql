f4f183596ddda811ecf4fb55348a52b7aaf2259c
Dhirana Dev <dhirana00@gmail.com>
2024-10-29 18:57:31 +0800

    into to sql lesson blank

--- DAC-004_Intro_to_SQL/DAC-004_Intro_to_SQL_Lesson_Blank.sql
+++ DAC-004_Intro_to_SQL/DAC-004_Intro_to_SQL_Lesson_Blank.sql
@@ -4,6 +4,14 @@
 
 -- Select department table, the employee table and vendor table. Let's explore the database a little!
 
+SELECT * 
+FROM humanresources.shift
+
+SELECT *
+FROM humanresources.employee
+
+SELECT * 
+FROM purchasing.vendor
 
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -13,6 +21,12 @@
 -- Select only name, start time and end time.
 
 
+SELECT 
+	name,
+	starttime,
+	endtime
+FROM humanresources.shift;
+
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -20,7 +34,11 @@
 
 -- Distinct group names from department and businessentityid from jobcandidate
 
+SELECT DISTINCT groupname
+FROM humanresources.department
 
+SELECT DISTINCT businessentityid
+FROM humanresources.jobcandidate
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -34,6 +52,14 @@
 
 -- Limit the table productvendor to 10 rows and purchaseorderdetail to 100 rows
 
+SELECT *
+FROM purchasing.productvendor
+LIMIT 10;
+
+
+SELECT *
+FROM purchasing.purchaseorderdetail
+LIMIT 100;
 
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -42,6 +68,11 @@
 
 -- From the customer table Multiplcation/division/addition/subtraction the store_id
 
+SELECT
+	customerid,
+	storeid * 10 AS tenfold
+FROM sales.customer
+LIMIT 15;
 
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -49,42 +80,86 @@
 --Q1: SELECT the DISTINCT title, last name, middlename and first_name of each person from the person schema. Return only 231 rows.
 --A1;
 
+SELECT DISTINCT
+	title,
+	lastname,
+	middlename,
+	firstname
+FROM person.person
+LIMIT 231;
 
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- WHERE clause: = 
+-- From employee table in humanresources schema, find out info on jobtitle, maritalstatus and gender
 -- gender is male
+
+SELECT
+	jobtitle,
+	maritalstatus,
+	gender 
+FROM humanresources.employee
+WHERE gender = 'M';
 
 
 -- Only Research and Development
 
+SELECT *
+FROM humanresources.department
+WHERE groupname = 'Research and Development'
+
 
 -- When dealing with NULL values
 
+SELECT *
+FROM purchasing.productvendor
+WHERE onorderqty IS NULL;
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- WHERE clause: Arithmetic filter
 
--- From customer table, territoryid = 4
 
+-- From customer table, territoryid = 4
+SELECT *
+FROM sales.customer
+WHERE territoryid = 4
+LIMIT 100;
 
 -- From person table, emailpromotion <> 0
+SELECT *
+FROM person.person
+WHERE emailpromotion <> 0
+LIMIT 50;
 
 
 -- From employee table, vacationhours >= 99
+SELECT *
+FROM humanresources.employee
+WHERE vacationhours >= 0
+LIMIT 50;
 
 
 -- From employee table, sickleavehours <= 20
-
+SELECT *
+FROM humanresources.employee
+WHERE sickleavehours <= 20
+LIMIT 50;
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 --WHERE clause: OR clause
 
+
+
 -- From employee table, select either Design Engineer or Tool Designer
 
+SELECT *
+FROM humanresources.employee
+WHERE jobtitle = 'Design Engineer'
+	OR jobtitle = 'Tool Designer';
+	
 
 -- From product, select either Black or Silver
 
@@ -93,7 +168,13 @@
 
 -- WHERE clause: AND clause
 
+
 -- From Vendor, preferredvendorstatus and activeflag must be TRUE
+
+SELECT *
+FROM purchasing.vendor
+WHERE preferredvendorstatus = TRUE
+	AND activeflag = TRUE
 
 
 -- From employee, gender must be Male and maritalstatus must be single
@@ -105,14 +186,25 @@
 
 -- From the employee table pick either, marital status as single and gender male or marital status as married and gender female.
 
+SELECT
+	jobtitle,
+	gender,
+	maritalstatus,
+	vacationhours,
+	sickleavehours
+FROM humanresources.employee
+WHERE (maritalstatus = 'S' AND gender = 'M')
+	OR (maritalstatus = 'M' AND gender = 'F');
+
 
 -- Example of poor formatting and logic.
--- From the salesperson table select territory_id either 4 or 6 and salesquota either 250000 or 300000
-
---
+-- From the salesperson table select territoryid either 4 or 6 and salesquota either 250000 or 300000
 
 
---
+SELECT *
+FROM sales.salesperson
+WHERE territoryid = 4 or territoryid = 6
+	AND salesquota = 250000 or salesquota = 300000;
 
 
 --Note: AND takes higher priority than OR
@@ -121,6 +213,11 @@
 -- The importance of having good SQL formatting when writing your SQL code.
 
 
+SELECT *
+FROM sales.salesperson
+WHERE (territoryid = 4 or territoryid = 6)
+	AND (salesquota = 250000 or salesquota = 300000);
+	
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -131,6 +228,13 @@
 -- '1984-04-30'
 -- '1985-05-04'
 
+SELECT *
+FROM humanresources.employee
+WHERE birthdate IN (
+	'1977-06-06',
+	'1984-04-30',
+	'1985-05-04'
+);
 
 
 -- Find all the middle names that contains either A or B or C.
